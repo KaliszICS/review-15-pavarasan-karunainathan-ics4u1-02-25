14ebcb784682ab73ceb3e8017e36dc168eb0a5a7
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-28 14:21:18 +0000

    Problem Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,18 @@
-public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+public class PracticeProblem{
+	static int goodString(String str){
+		int mincost = 0x3f3f3f3f;
+		for(int i = 0; i < str.length(); i++){
+			int lower = str.indexOf(str.charAt(i));
+			int upper = str.lastIndexOf(str.charAt(i));
+			if(upper != lower){
+				mincost = Math.min(mincost, lower + str.length()-1 - upper);
+			}
+		}
+		if(mincost == 0x3f3f3f3f){
+			mincost = -1;
+		}
+		return mincost;
 	}
 
+	public static void main(String args[]){}
 }
